0c876372945f28788330ecded88348b172dfa703
Nikita Nosov <shajtanuch@gmail.com>
2025-03-23 16:48:42 +0200

    Fix: added init branch as an exception

--- scripts/.github/validate-branch.sh
+++ scripts/.github/validate-branch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 branch_name=$(git symbolic-ref --short HEAD)
-if [[ ! "$branch_name" =~ ^(init|feature|fix|hotfix)/.* ]]; then
+if [[ ! "$branch_name" =~ ^(init|feature|fix|hotfix)/.* && "$branch_name" != "init" ]]; then
   echo "Branch name must start with 'init/', 'feature/', 'fix/', or 'hotfix/'"
   exit 1
 fi
