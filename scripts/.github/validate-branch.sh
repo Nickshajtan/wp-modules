97afa32161a7279939c570f476a3ed0f5f256d57
Nikita Nosov <shajtanuch@gmail.com>
2025-03-24 01:08:09 +0200

    Fix: to POSIX

--- scripts/.github/validate-branch.sh
+++ scripts/.github/validate-branch.sh
@@ -7,20 +7,22 @@ if [ -z "$GITHUB_REF" ]; then
 else
   # Running in GitHub Actions, use GITHUB_REF to get the branch name
     # If it's a pull request, we use GITHUB_HEAD_REF (the source branch of the PR)
-    if [[ "$GITHUB_REF" =~ ^refs/pull/ ]]; then
-      branch_name="$GITHUB_HEAD_REF"
-    else
-      branch_name=$(echo "$GITHUB_REF" | sed 's/refs\/heads\///')
-    fi
+    case "$GITHUB_REF" in
+        refs/pull/*) branch_name="$GITHUB_HEAD_REF" ;;
+        *) branch_name=$(echo "$GITHUB_REF" | sed 's|refs/heads/||') ;;
+    esac
 fi
 
 echo "Branch name '$branch_name'"
 
-if [[ "$branch_name" == "init" ]]; then
-  exit 0
-fi;
+case "$branch_name" in
+  init|main) exit 0 ;;
+esac
 
-if [[ ! "$branch_name" =~ ^(init|feature|fix|hotfix)/.* ]]; then
-  echo "Branch name must start with 'init/', 'feature/', 'fix/', or 'hotfix/'"
-  exit 1
-fi
+case "$branch_name" in
+  init/*|feature/*|fix/*|hotfix/*) ;;
+  *)
+    echo "Branch name must start with 'init/', 'feature/', 'fix/', or 'hotfix/'"
+    exit 1
+    ;;
+esac
--- scripts/.github/validate-commit.sh
+++ scripts/.github/validate-commit.sh
@@ -2,12 +2,15 @@
 commit_msg_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
 # Running in GitHub Actions, so skip if merging
-if [[ ! "$commit_msg_lower" =~ ^(merge|rebase): ]]; then
-  exit 0
-fi
+case "$commit_msg_lower" in
+  merge:*|rebase:*) exit 0 ;;
+esac
 
 # Running locally
-if [[ ! "$commit_msg_lower" =~ ^(feat|fix|hotfix|test): ]]; then
-  echo "Commit message must start with 'feat:', 'fix:', 'test:', or 'hotfix:'"
-  exit 1
-fi
+case "$commit_msg_lower" in
+  feat:*|fix:*|hotfix:*|test:*) ;;
+  *)
+    echo "Commit message must start with 'feat:', 'fix:', 'test:', or 'hotfix:'"
+    exit 1
+    ;;
+esac
