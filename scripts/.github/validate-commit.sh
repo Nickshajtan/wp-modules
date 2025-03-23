a9d4652ec84f9e14bec0c1df02610785d3f3cc95
Nikita Nosov <shajtanuch@gmail.com>
2025-03-23 17:10:40 +0200

    Fix: corrected workflow

--- scripts/.github/validate-commit.sh
+++ scripts/.github/validate-commit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 commit_msg=$(cat "$1")
-if [[ ! "$commit_msg" =~ ^(Feat|Fix|Hotfix): ]]; then
-  echo "Commit message must start with 'feat:', 'fix:', or 'hotfix:'"
+commit_msg_lower=$(echo "$commit_msg" | tr '[:upper:]' '[:lower:]')
+
+if [[ ! "$commit_msg_lower" =~ ^(feat|fix|hotfix|test): ]]; then
+  echo "Commit message must start with 'feat:', 'fix:', 'test:', or 'hotfix:'"
   exit 1
 fi
