9d9c39d892bfac3c3fb0899693937aff5055a17a
Nikita Nosov <shajtanuch@gmail.com>
2025-03-23 16:55:23 +0200

    Fix: added condition to check branch name properly

--- scripts/.github/validate-branch.sh
+++ scripts/.github/validate-branch.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-branch_name=$(git symbolic-ref --short HEAD)
+
+# Check if running in GitHub Actions or locally
+if [ -z "$GITHUB_REF" ]; then
+  # Running locally, use git symbolic-ref to get the branch name
+  branch_name=$(git symbolic-ref --short HEAD)
+else
+  # Running in GitHub Actions, use GITHUB_REF to get the branch name
+  branch_name=$(echo "$GITHUB_REF" | sed 's/refs\/heads\///')
+fi
+
 if [[ ! "$branch_name" =~ ^(init|feature|fix|hotfix)/.* && "$branch_name" != "init" ]]; then
   echo "Branch name must start with 'init/', 'feature/', 'fix/', or 'hotfix/'"
   exit 1
