2043dfa3a41a204d066dfbbf8bcc0a275bbd2826
Nikita Nosov <shajtanuch@gmail.com>
2025-08-07 18:10:07 +0300

    Feat: added symfony engine test

---
+++ composer-packages/hcc-events-system-library/tests/unit/engine/SymfonyEngineTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use HCC\Events\Event;
+use \Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use HCC\Events\Engine\SymfonyEngine;
+
+class SymfonyEngineTest extends TestCase
+{
+    public function testDispatchSyncCallsDispatcher(): void
+    {
+        $event = new Event(
+            eventName: 'foo_event',
+            callback: fn () => 'bar'
+        );
+        $dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $dispatcher->expects($this->once())->method('dispatch')->with($event);
+        $engine = new SymfonyEngine($dispatcher);
+        $engine->dispatchSync($event);
+    }
+}
